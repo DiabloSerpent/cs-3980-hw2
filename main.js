290eccfec689325ddfa87b0350cf0c71151185d5
Max Herrera <mh1101110@gmail.com>
2026-03-05 18:32:47 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+{
+    const popDataUrl = "https://api.datausa.io/tesseract/data.jsonrecords?cube=acs_yg_total_population_5&measures=Population&drilldowns=Year";
+
+    fetch(popDataUrl).then(r => {
+        return r.json();
+    }).then(allData => {
+        let table = document.getElementById("pop-data");
+        table.innerHTML = "";
+        allData.data.forEach(row => {
+            table.innerHTML += renderRow(row.Year, row.Population);
+        });;
+    });
+
+    function renderRow(yr,pop) {
+        return `
+        <tr>
+            <th>${yr}</th>
+            <th>${pop}</th>
+        </tr>
+        `;
+    }
+}
